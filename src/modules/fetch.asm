be1aae28ef8d95d2aed689adecf42a093d2ab0dc
skalerozik <enokikimidzake@gmail.com>
2026-07-11 16:53:49 +0900

    upd: fetch.asm

--- src/modules/fetch.asm
+++ src/modules/fetch.asm
@@ -1,11 +1,10 @@
 sysart      db '', 0
 systeminfo  db 10, 13
             db 'korpOS Aster [x16 Edition]', 10, 13
-            db 'Version: 1.0', 10, 13
-            db 'Kernel: korpcore-0.10-generic', 10, 13
+            db 'Version: 0.0.1', 10, 13
+            db 'Kernel: mandarn', 10, 13
             db 'Display: 80x25', 10, 13
-            db 'Shell: korpTerm', 10, 13
-            db 'Nazar Prokudin. 2024-2025', 10, 13
+            db 'skalerozik. 2024-2026', 10, 13
             db 0
 
 do_sysinf:
